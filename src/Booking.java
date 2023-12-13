515b13959250dada6ed5e5b92a58fe4ee861b2a9
jennifer.nord <jennifer.nord@elev.ga.ntig.se>
2023-12-13 11:04:53 +0100

    Har börjat korskoppla två klasser

--- src/Booking.java
+++ src/Booking.java
@@ -1,3 +1,16 @@
-public class Booking {
+import java.util.Scanner;
 
+public class Booking {
+    Scanner scan = new Scanner(System.in);
+    private Guest guest;
+
+    Booking(Guest guest){
+        this.guest = guest;
+    }
+
+    public void addGuest(String guestName){
+        System.out.println("Enter name of guest: ");
+        guestName = scan.nextLine();
+
+    }
 }
--- src/Cash.java
+++ src/Cash.java
@@ -2,6 +2,6 @@ public class Cash {
     private int amount;
 
     Cash(int amount){
-
+        this.amount = amount;
     }
 }
